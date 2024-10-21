2278d7d01209ca779ba6fadd197f3fd5cf99b0e7
LUCKY FERNANDEZ HADUCA <147017329+HaducaLucky@users.noreply.github.com>
2024-10-21 19:15:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -33,9 +33,74 @@
 
 #TODO: Another Ex.
 
-online = False
+# online = True
 
-if online:
-    print("The user is online")
+# if online:
+#     print("The user is online")
+# else:
+#     print("The user is offline")
+
+#? Exercise
+
+# age = int(input("How old are you?: "))
+
+# if age < 13:
+#     category = "child"
+# elif 13 <= age <= 19:
+#     category = "teenageer"
+# elif 20 <= age <= 59:
+#     category = "adult"
+# else:
+#     category = "senior"
+
+# print(f"You are a {category}")
+
+#? Exercise
+
+# grade = int(input("Enter you grade (0-100): "))
+
+# if 90 <= grade <= 100:
+#     print("A")
+# elif 80 <= grade <= 89:
+#     print("B")
+# elif 70 <= grade <= 79:
+#     print("C")
+# elif 60 <= grade <= 69:
+#     print("D")
+# elif 0 <= grade <= 59:
+#     print("F")
+# else:
+#     print("Invalid grade! Please enter a grade between 0 and 100 only!")
+
+#? Exercise
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print(f"{number} is an even number")
+# else:
+#     print(f"{number} is an odd number")
+
+#? Exercise
+
+# password = input("Enter your password: ")
+
+# if password == "password123":
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+#? Exercise
+
+age = int(input("Enter your age: "))
+
+if age >= 65:
+    print("$10")
+elif 13 <= age <= 64:
+    print("$15")
+elif 6 <= age <= 12:
+    print("$10")
+elif 0 <= age <= 5:
+    print("Free")
 else:
-    print("The user is offline")
+    print("Enter a proper age!")
